a2c641a6ac46d80a514b382275f57423afe9bc06
Anas Bajjouk <bajjouk.a@office.lan>
2020-08-10 16:59:41 +0200

    added var colors in css, added sidebar compoenent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <div>
+      <h1>HI</h1>
 
+      {/* Sidebar */}
+      <Sidebar />
+      
+      {/* Feed */}
+
+      {/* Widgets */}
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,39 @@
+import React from "react";
+//COMPONENTS
+import SidebarOption from "../sidebarOption/SidebarOption";
+
+//STYLING
+import "./Sidebar.css";
+
+//ICONS
+import TwitterIcon from "@material-ui/icons/Twitter";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+
+const Sidebar = () => {
+  return (
+    <div>
+      {/* Twitter icon */}
+      <TwitterIcon />
+      
+      <SidebarOption text="Home" Icon={HomeIcon} />
+      <SidebarOption text="Explore" Icon={SearchIcon} />
+      <SidebarOption text="Notifications" Icon={NotificationsNoneIcon} />
+      <SidebarOption text="Messages" Icon={MailOutlineIcon} />
+      <SidebarOption text="Bookmarks" Icon={BookmarkBorderIcon} />
+      <SidebarOption text="Lists" Icon={ListAltIcon} />
+      <SidebarOption text="Profile" Icon={PermIdentityIcon} />
+      <SidebarOption text="More" Icon={MoreHorizIcon} />
+
+      {/* Button => tweet */}
+    </div>
+  );
+};
+
+export default Sidebar;
